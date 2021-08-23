dfcc9d28aa3188347bf4dcdf255243eefc300eb8
Pier <p.delaunay1@gmail.com>
2021-08-23 12:03:30 +0400

    init

---
+++ pages/fun.vue
@@ -0,0 +1,4 @@
+<template>
+    <!-- <h2> titre deux </h2> -->
+    <NuxtLink to="/">to Home page</NuxtLink>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <center>
+
+    <NuxtLogo/>
+    <h1>Home page</h1>
+    <NuxtLink to="/fun">
+      <button type="button" class="btn btn-success">Voir page deux</button>        
+    </NuxtLink>
+    <a class='btn btn-primary' href="https://nuxtjs.org">Lien vers une page externe</a>
+
+    </center>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
