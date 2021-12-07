24cb941a868c09cf6caa57574e8ede10fcaaea73
Pier <p.delaunay1@gmail.com>
2021-12-07 19:12:22 +0400

    adding layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+
+
+
+<template>
+
+<!-- doc des layout : https://nuxtjs.org/docs/directory-structure/layouts/ -->
+
+    <main>
+        <img src="https://avatars.githubusercontent.com/u/1768831?v=4" alt="">
+        <Nuxt />
+    </main>        
+</template>
+
+<style scoped>
+    
+    img{
+        width: 50px;
+        border-radius: 50px;
+        position: relative;
+        top: 20px;
+        left: 20px;
+    }
+    
+</style>
