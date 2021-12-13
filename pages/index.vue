98e1c023dda6bc48d7f9d3d177713a0facd4e0eb
Pier <p.delaunay1@gmail.com>
2021-12-13 14:20:38 +0400

    Update index.vue
    debug warning

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,10 @@
     <Menu/>
     <Titre/>
     
+
+    
    
-    <!-- <h1>Home page</h1> -->
-    <a class='btn btn-primary' href="https://github.com/gitkyo/starter-nuxt">Mon dépot github</a>
+   
 
     
 
