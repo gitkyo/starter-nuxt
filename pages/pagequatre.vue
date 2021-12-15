ba0cc6d88c76f0e364bd8cdb318cb62ea8fdc243
Pier <p.delaunay1@gmail.com>
2021-12-15 13:54:00 +0400

    contact form integration 6

--- pages/pagequatre.vue
+++ pages/pagequatre.vue
@@ -14,13 +14,38 @@
 				<div class="offset-md-4 col-md-4">
 					
                     
-                    <form name="contactus" netlify netlify-honeypot="bot-field" hidden>
-                        <input type="text" name="name" />
-                        <input type="radio" name="email" />
+                    <!-- <form name="contactus" netlify netlify-honeypot="bot-field" hidden>
+                        nom : <input type="text" name="name" />
+                        email : <input type="radio" name="email" />
                         <textarea name="message"></textarea>
+                    </form> -->
+
+                    <form
+                        name="ask-question"
+                        method="post"
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field"
+                        >
+                        <input type="hidden" name="form-name" value="ask-question" />
+                        <label v-for="(panelist, index) in panelists" :key="index">
+                            <input
+                            type="radio"
+                            name="panelist"
+                            :value="panelist"
+                            @input="ev => updatePanelist"
+                            :checked="panelist === currentPanelist"
+                            />
+                            <span>{{ panelist }}</span>
+                        </label>
+                        <div class="form-group">
+						  <label for="exampleInputEmail1">Votre Email</label>
+						  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+						  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+                        <button>Submit</button>
                     </form>
-                    <!-- 
-                    <form name="contact" method="POST" data-netlify="true">
+                    
+                    <!-- <form name="contact" netlify netlify-honeypot="bot-field">
 						<div class="form-group">
 						  <label for="exampleInputEmail1">Votre Email</label>
 						  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -34,8 +59,8 @@
 							
 						</p>
 						<button type="submit" class="btn btn-primary">Envoyer</button>
-					</form> 
-                    -->
+					</form>  -->
+                   
 
                     
 				</div>				
